df229e5c3c8eba46f14f4eaba739156ead5a42ce
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-01 16:22:11 -0400

    Update file.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path')
 app.use(express.json())
 
 const userRoutes = require("./server/routes/user") //access routes in user
+const eventRoutes = require("./server/routes/event")
+const taskRoutes = require("./server/routes/task")
 
 //CORS middleware
 app.use(function(req, res, next) {
@@ -17,7 +19,9 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + "/public"))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')))
 
-app.use('/user', userRoutes) //call to use fetch and make http requests
+app.use("/user", userRoutes) //call to use fetch and make http requests
+app.use("/event", eventRoutes)
+app.use("/task", taskRoutes)
 
 const PORT = process.env.PORT || 3000 //set a communication endpoint
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`)) //start server
