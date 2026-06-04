eb988977fc86f6ae92a1924ef66c04130740779b
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-04 07:56:05 -0400

    Add databases to test connection.

---
+++ server/models/event.js
@@ -0,0 +1,21 @@
+//fake database
+const event = [
+    {
+        eventName: "event",
+        eventDate: {
+            month: "June",
+            day: 4,
+            year:2026
+        },
+        eventTime: "12:00am",
+        eventLocation: "eventAddress",
+        eventDescription: "This is an event description.",
+        userId: 1
+    }
+]
+
+function getEvent() {
+    return getEvent;
+}
+
+module.exports = { getEvent }
---
+++ server/models/task.js
@@ -0,0 +1,14 @@
+//fake database
+const task = [
+    {
+        taskName: "task",
+        taskDescription: "This is a task description.",
+        userId: 1
+    }
+]
+
+function getTask() {
+    return getTask;
+}
+
+module.exports = { getTask }
