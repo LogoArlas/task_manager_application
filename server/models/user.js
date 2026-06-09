8982b281ffbd13f6c8046daa43d62051e61e601d
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-09 16:10:10 -0400

    Create schema for user. Add CRUD operations.

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,61 @@
+const mongoose = require("mongoose")
+
+//schema
+const userSchema = new mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    email: String
+})
+
+//model of schema
+const User = mongoose.model("User", userSchema)
+
+//CRUD functions on model
+//CREATE a user
+async function register(username, password, email) {
+    const user = getUser(username)
+    if(user) throw Error('Username already in use')
+    
+    const newUser = await User.create({
+        username: username,
+        password: password
+    })
+}
+//READ a user
+async function login(username, password) {
+    const user = getUser(username)
+    if(!user) throw Error('User not found')
+    if(user.password != password) throw Errow('Wrong Password')
+
+    return user
+}
+
+//UPDATE
+/*async function updatePassword(id, password) {
+    const user = await User.findByIdAndUpdate()
+}*/
+
+async function updatePassword(id, password) {
+    const user = await User.updateOne({"_id": id}, {$set: {}})
+    return user
+}
+
+//DELETE
+async function deleteUser(id) {
+    await User.deleteOne({"_id": id})
+}
+
+//helper function
+async function getUser(username) {
+    return await User.findOne({"username": username})
+}
+
+//export function to access in routes
+module.exports = { register, login, updatePassword, deleteUser}
+
 //fake database for testing connection
 
-const users = [
+/*const users = [
     {
         username: "hello1",
         password: "password"
@@ -20,4 +75,4 @@ function getUsers() {
     return users;
 }
 
-module.exports = { getUsers }
+module.exports = { getUsers }*/
