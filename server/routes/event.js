e71f430a29fb99e9122d1eb94c9bc628649a52a7
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-11 08:44:54 -0400

    Add routes for event entity.

--- server/routes/event.js
+++ server/routes/event.js
@@ -2,13 +2,42 @@ const express = require("express")
 const Event = require("../models/event")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    try {
-        const event = Event.getEvent()
-        res.send(event)
-    } catch(err) {
-        res.status(401).send({message: err.message})
-    }
+router
+    .post("/createEvent", (req, res) => {
+      try {
+          const event = await Event.createEvent(req.body.eventName, req.body.eventDate, req.body.eventTime, req.body.eventLocation,
+          req.body.event.Description, req.body.userId)
+          res.send({...event, userId: undefined})
+      } catch(error) {
+          res.status(401).send({message: error.message})
+      }
 })
+
+    .get('/getEvent', (req, res) => {
+        try {
+            const event = await Event.getEvent(req.body.eventName)
+            res.send({...event, userId: undefined})
+        } catch(error) {
+            res.status(401).send({message: error.message})
+        }
+    })
+
+    .put('/update', (req, res) => {
+        try {
+            const event = await Event.updateEventLocation(req.body.id, req.body.eventLocation)
+            res.send({...event, user: undefined})
+        } catch(error) {
+            res.status(401).send({message: error.message})
+        }
+    })
+
+    .delete('/delete', (req, res) => {
+        try {
+            await Event.deleteEvent(req.body.id)
+            res.send({success: 'Event deleted'})
+        } catch(error) {
+            res.status(401).send({message: error.message})
+        }
+    })
 
 module.exports = router
