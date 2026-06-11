eeb91fa442402ceb60ba81aaf9f9863eab4bf7a0
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-11 09:31:21 -0400

    Add routes for task entity.

--- server/routes/task.js
+++ server/routes/task.js
@@ -2,13 +2,41 @@ const express = require("express")
 const Task = require("../models/task")
 const router = express.Router()
 
-router.get("/", (req, res) => {
+router
+    .post('/createTask', async (req, res) => {
     try {
-        const task = Task.getTask()
+        const task = await Task.getTask(req.body.taskName, req.body.taskDescription, req.body.userId)
         res.send(task)
-    } catch(err) {
-        res.status(401).send({message: err.message})
+    } catch(error) {
+        res.status(401).send({message: error.message})
     }
 })
+
+    .get('/getTask', async (req, res) => {
+    try {
+        const task = await Task.getTask(req.body.taskName)
+        res.send(task)
+    } catch(error) {
+        res.status(401).send({message: error.message})
+    }
+})
+
+    .put('/update', async (req, res) => {
+    try {
+        const task = await Task.updateTaskName(req.body.id, req.body.eventName)
+        res.send(task)
+    } catch(error) {
+        res.status(401).send({message: error.message})
+    }
+})
+
+    .delete('/delete', async (req, res) => {
+    try {
+        await Task.deleteEvent(req.body.id)
+        res.send({success: 'Task deleted'})
+    } catch(error) {
+        res.status(401).send({message: error.message})
+     }
+    })
 
 module.exports = router
