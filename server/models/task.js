a29fb5c246e1e209fcc32cc1423ea064c2529d58
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-11 09:15:54 -0400

    Add schema for task entity. Add functions for CRUD operations.

--- server/models/task.js
+++ server/models/task.js
@@ -1,5 +1,44 @@
+const mongoose = require("mongoose")
+
+const taskSchema = new mongoose.Schema({
+    taskName: {type: String, unquie: true, required: true},
+    taskDescription: {type: String},
+    userId: [
+        {type: mongoose.Schema.Types.ObjectId,
+        ref: "Event"
+    }]
+})
+
+const Task = mongoose.model("Task", taskSchema)
+
+async function createTask(taskName, taskDescription) {
+    const newTask = await Task.create({
+    taskName: taskName,
+    taskDescription: taskDescription,
+    userId: userId
+    })
+    return newTask._doc
+}
+
+async function getTask() {
+    if(!task) throw Error('Task not found')
+
+    return task._doc
+}
+
+async function updateTaskName() {
+    const task = await Task.findByIdAndUpdate(id, {password}, {new: true})
+  return task._doc
+}
+
+async function deleteTask(id) {
+  await Task.deleteOne({"_id": id})
+};
+
+module.export = { createTask, getTask, updateTaskName, deleteTask }
+
 //fake database
-const task = [
+/* const task = [
     {
         taskName: "task",
         taskDescription: "This is a task description.",
@@ -11,4 +50,4 @@ function getTask() {
     return task;
 }
 
-module.exports = { getTask }
+module.exports = { getTask }*/
