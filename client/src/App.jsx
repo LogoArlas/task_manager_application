6c7866a7697506b7416d8c73c269c9fce5e53ffd
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-23 09:47:23 -0400

    Delete import statement for useState, and delete HTML section element.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Register from './components/Register.jsx'
 import Login from './components/Login.jsx'
 import Navbar from './components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
         <div className="App">
           Welcome to the App.
           <Register />
           <Login />
           <Navbar />
         </div>
-      </section>
     </>
   )
 }
