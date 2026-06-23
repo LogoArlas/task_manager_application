6da81c4a10e8fa24e6da4f7b3a4fd0e53c2dede6
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-23 10:57:19 -0400

    Uncomment import statement for createRoot.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-//import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
