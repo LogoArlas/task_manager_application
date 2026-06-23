fef4c601470b796450a297f57e49e5d14c62e543
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-23 09:31:50 -0400

    Add components for User entity to App.jsx file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import Register from './components/Register.jsx'
+import Login from './components/Login.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
