3da9cbf15135593c506194ec19d070c0457a3a9e
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-23 22:51:15 -0400

    Fix syntax on import statement for ReactDOM. Delete import statements for StrictMode and createRoot.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { StrictMode } from 'react'
-import { ReactDOM } from 'react-dom/client'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
