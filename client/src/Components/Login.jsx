65c17e41c480c9cb9e43860af81f9738529e25eb
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-26 15:20:06 -0400

    Update layout and add box shadow.

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,33 +1,39 @@
 const Login = () => {
     return(
-        <div>
-        <div className="card border-secondary mb-3">
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">Username</label>
-                    <input 
-                    type="text" 
-                    className="form-control" 
-                    id="username" 
-                    name='username'
-                    required
-                    />
+        <div className="container">
+            <div className="row">
+                <div classname="col-mx">
+                    <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                        <div className="card border-secondary p-3 mb-3">
+                            <div className="card card-header">Login</div>
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="username" className="form-label">Username</label>
+                                    <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    id="username" 
+                                    name='username'
+                                    required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id='password'
+                                    required
+                                    />
+                                </div>
+                                <input type="submit" className="btn btn-primary" value="Register"/>
+                                <div id="loginHelp" className="form-text">Create an account.</div>
+                                <p><a class="link-opacity-100" href="#">Register</a></p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    id='password'
-                    required
-                    />
-                </div>
-                <input type="submit" className="btn btn-primary" value="Register"/>
-                <div id="loginHelp" className="form-text">Create an account.</div>
-                <p><a class="link-opacity-100" href="#">Register</a></p>
-            </form>
-        </div>
-        <div className="card-footer text-body-secondary">Footer</div>
+            </div>
         </div>
     )
 }
