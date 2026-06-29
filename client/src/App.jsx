82a8a116d7ed243f04cbbf570c3d979170a00220
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-29 16:49:09 -0400

    Import BrowserRouter, Routes, Route, Event, and Task. Add routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
-import Register from './components/Register.jsx'
-import Login from './components/Login.jsx'
+import Register from './components/pages/Register.jsx'
+import Login from './components/pages/Login.jsx'
 import Navbar from './components/Navbar.jsx'
+import Event from './components/pages/Event.jsx'
+import Task from './components/pages/Task.jsx'
 
 function App() {
   return (
       <div className="App">
-        <Navbar />
-        <Register />
-        <Login />
-      </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navbar />} >
+              <Route index element={<Register />} />
+              <Route path="login" element={<Login />} />
+              <Route path="event" element={<Event />} />
+              <Route path="register" element={<Task />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
