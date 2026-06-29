14763d828b7aa04b8d5efed245fee2c34fd1ba60
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-29 19:21:34 -0400

    Remove link.

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="nav-link" to="/register">Task Manager</Link>
+                    <div className="navbar-brand">Task Manager</div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
