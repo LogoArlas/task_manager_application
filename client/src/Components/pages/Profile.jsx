6b97d8871c8b1fb6db11fd4d5540fdad91c22a76
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-30 19:25:46 -0400

    Put Event and Task components on Profie page.

--- client/src/Components/pages/Profile.jsx
+++ client/src/Components/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 import Login from "./Login.jsx"
+import Event from '../Event.jsx'
+import Task from '../Task.jsx'
 
 const Profile = () => {
     return (
         <div className="container-fluid">
             <p> Profile Page</p>
+            <Event />
+            <Task />
         </div>
     )
 }
