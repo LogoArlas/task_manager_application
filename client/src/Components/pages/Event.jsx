e267123b8b8e19d118cd431712c168daf97062b7
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-30 08:00:24 -0400

    Add card border and shadow around card.

--- client/src/Components/pages/Event.jsx
+++ client/src/Components/pages/Event.jsx
@@ -29,66 +29,71 @@ const Event = () => {
         <div className="container">
             <div className="row">
                 <div className="col">
-                    <form onSubmit={onSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="eventName" className="form-label">Event Name</label>
-                            <input 
-                            type="text" 
-                            className="form-control" 
-                            id="eventName"
-                            name='eventName'
-                            onChange={onChange}
-                            value={eventName}
-                            required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="eventDate" className="form-label">Event Date</label>
-                            <input 
-                            type="text" 
-                            className="form-control" 
-                            id="eventDate"
-                            name='eventDate'
-                            onChange={onChange}
-                            value={eventDate}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="eventTime" className="form-label">Event Time</label>
-                            <input 
-                            type="text" 
-                            className="form-control" 
-                            id="eventTime"
-                            name='eventTime'
-                            onChange={onChange}
-                            value={eventTime}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="eventLocation" className="form-label">Event Location</label>
-                            <input 
-                            type="text" 
-                            className="form-control" 
-                            id="eventLocation"
-                            name='eventLocation'
-                            onChange={onChange}
-                            value={eventLocation}
-                            />
-                        </div>
-                        <div class="mb-3">
-                            <label htmlFor="eventDescription" className="form-label">Description</label>
-                            <textarea 
-                            className="form-control" 
-                            id="eventDescription" 
-                            rows="3"
-                            name='eventDescription'
-                            onChnage={onChange}
-                            value={eventDescription}>
-                            </textarea>
-                        </div>
-                        <button type="submit" className="btn btn-success">Add</button>
-                    </form>
-                </div>
+                    <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                        <div className="card border-secondary p-3 mb-3">
+                            <div className="card-header">Event</div>
+                                <form onSubmit={onSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="eventName" className="form-label">Event Name</label>
+                                        <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        id="eventName"
+                                        name='eventName'
+                                        onChange={onChange}
+                                        value={eventName}
+                                        required
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="eventDate" className="form-label">Event Date</label>
+                                        <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        id="eventDate"
+                                        name='eventDate'
+                                        onChange={onChange}
+                                        value={eventDate}
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="eventTime" className="form-label">Event Time</label>
+                                        <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        id="eventTime"
+                                        name='eventTime'
+                                        onChange={onChange}
+                                        value={eventTime}
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="eventLocation" className="form-label">Event Location</label>
+                                        <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        id="eventLocation"
+                                        name='eventLocation'
+                                        onChange={onChange}
+                                        value={eventLocation}
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label htmlFor="eventDescription" className="form-label">Description</label>
+                                        <textarea 
+                                        className="form-control" 
+                                        id="eventDescription" 
+                                        rows="3"
+                                        name='eventDescription'
+                                        onChnage={onChange}
+                                        value={eventDescription}>
+                                        </textarea>
+                                    </div>
+                                <button type="submit" className="btn btn-success">Add</button>
+                                </form>
+                            </div>
+                        </div> {/* shadow */}
+                    </div>
                 <div className="col"></div>
             </div>
         </div>
--- client/src/Components/pages/Task.jsx
+++ client/src/Components/pages/Task.jsx
@@ -30,32 +30,37 @@ const Task = () => {
         <div className="container">
             <div className="row">
                 <div className="col-3">
-                    <form onSubmit={onSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="taskName" className="form-label">Task Name</label>
-                            <input 
-                            type="text" 
-                            className="form-control" 
-                            id="taskName"
-                            name='taskName'
-                            onChange={onChange}
-                            value={taskName}
-                            required
-                            />
+                    <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+                        <div className="card border-secondary p-3 mb-3">
+                            <div className="card-header">Task</div>
+                            <form onSubmit={onSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="taskName" className="form-label">Task Name</label>
+                                    <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    id="taskName"
+                                    name='taskName'
+                                    onChange={onChange}
+                                    value={taskName}
+                                    required
+                                    />
+                                </div>
+                                <div class="mb-3">
+                                    <label htmlFor="taskDescription" className="form-label">Description</label>
+                                    <textarea 
+                                    className="form-control" 
+                                    id="taskDescription" 
+                                    rows="3"
+                                    name='taskDescription'
+                                    onChange={onChange}
+                                    value={taskDescription}>
+                                    </textarea>
+                                </div>
+                                <button type="submit" className="btn btn-success">Add</button>
+                            </form>
                         </div>
-                        <div class="mb-3">
-                            <label htmlFor="taskDescription" className="form-label">Description</label>
-                            <textarea 
-                            className="form-control" 
-                            id="taskDescription" 
-                            rows="3"
-                            name='taskDescription'
-                            onChange={onChange}
-                            value={taskDescription}>
-                            </textarea>
-                        </div>
-                        <button type="submit" className="btn btn-success">Add</button>
-                    </form>
+                    </div>
                 </div>
                 <div className="col"></div>
             </div>
