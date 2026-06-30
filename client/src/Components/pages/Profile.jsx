43a6cd2ae379565e20e682fc386a5a4b44b4c1ac
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-06-29 20:39:47 -0400

    Add profile component.

---
+++ client/src/Components/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import { Login } from './Login.jsx'
+import { useState } from "react"
+
+const Profile = () => {
+    return (
+        <div className="container">
+            <p>Welcome, {user.username}</p>
+        </div>
+    )
+}
+
+export default Profile
